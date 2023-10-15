822487d355e33608db0ccf556f63a36392536012
Jemima Tio <123765572+JemimaJT@users.noreply.github.com>
2023-10-15 18:32:05 +0000

    hide navbar

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 const YEAR = new Date().getFullYear()
 
 export default {
-  navbar.component: (hidden) 
+  navbar.component: (false) 
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Jemima Tio.
