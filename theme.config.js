26f44a8dd8f137b990467e1fd00fe7cac0cb3425
Jemima Tio <123765572+JemimaJT@users.noreply.github.com>
2023-10-15 19:01:12 +0000

    add theme to home page

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,6 @@
 const YEAR = new Date().getFullYear()
 
 export default{
-  darkMode: true
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Jemima Tio.
